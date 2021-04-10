c47b4968d3c0ddd97a8a27c5ee47926e13b502e4
Fernando de Alcântara Cadalso <fernando.cadalsoii@gmail.com>
2021-04-09 23:19:01 -0300

    Deploy atual

--- src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -2,7 +2,6 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
